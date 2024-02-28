c7db5018ad416b8915ff5134e35b7a51a64ca784
vitorgabrielazevedo <137099376+vitorgabrielazevedo@users.noreply.github.com>
2024-02-28 10:40:10 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,12 @@
+// selecionando dados
+const previa = document.querySelector("#previa");
+const operacaoAtual = document.querySelector("operacaoAtual");
+const botaoIgual = document.querySelector(".igual");
+const botaoPonto = document.querySelector(".ponto");
+const botoesOperador = document.querySelectorAll(".operador");
+const botoesOperacao = document.querySelectorAll(".operacao");
+const botoesNumeros = document.querySelectorAll(".num");
+
+// funções
+
+// eventos
