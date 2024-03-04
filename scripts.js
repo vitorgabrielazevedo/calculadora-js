4409a064c130eafe3a493993d0e4c9762955c7a9
vitorgabrielazevedo <137099376+vitorgabrielazevedo@users.noreply.github.com>
2024-03-04 10:46:02 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,12 +1,102 @@
-// selecionando dados
-const previa = document.querySelector("#previa");
-const operacaoAtual = document.querySelector("operacaoAtual");
-const botaoIgual = document.querySelector(".igual");
-const botaoPonto = document.querySelector(".ponto");
-const botoesOperador = document.querySelectorAll(".operador");
-const botoesOperacao = document.querySelectorAll(".operacao");
-const botoesNumeros = document.querySelectorAll(".num");
+const displayPrevia = document.querySelector("#previa")
+const displayAtual = document.querySelector("#operacaoAtual")
+const botaoIgual = document.querySelector(".igual")
+const botaoPonto = document.querySelector(".ponto")
+const botoesNum = document.querySelectorAll(".num")
+const botoesOperador = document.querySelectorAll(".operador")
+const botoesOperacao = document.querySelectorAll(".operacao")
 
-// funções
+class Calculadora {
+    constructor(displayPrevia, displayAtual){
+        this.displayPrevia = displayPrevia
+        this.displayAtual = displayAtual
+        this.operacaoAtual = "";
+    }
 
-// eventos
+    adicionarDigito(digito){
+        if(digito === "." && this.operacaoAtual.includes(".")){
+            return
+        }
+            
+        this.operacaoAtual = digito 
+        this.atualizarDisplay()
+    }
+
+    calcular(digito){
+        let resultado = eval(this.displayAtual.innerText);
+        this.displayPrevia.innerText = this.displayAtual.innerText
+        this.displayAtual.innerText = resultado
+    }
+
+    processarDelOp(){
+        this.displayAtual.innerText = this.displayAtual.innerText.slice(0, -1)
+        this.displayPrevia.innerText = this.displayPrevia.innerText.slice(0, -1)
+    }
+
+    processarCOp(){
+        this.displayAtual.innerText = ""
+    }
+
+    processarCEOp(){
+        this.displayAtual.innerText = ""
+        this.displayPrevia.innerText = ""
+    }
+
+    processarOperacoes(valor){
+        if(valor === "DEL"){
+            this.processarDelOp()
+        }else if(valor === "C"){
+            this.processarCOp()
+        }else{
+            this.processarCEOp()
+        }
+    }
+
+    atualizarDisplay(){ 
+        this.displayAtual.innerText += this.operacaoAtual
+        //this.displayPrevia.innerText = this.displayAtual.innerText
+    }
+}
+
+let sinal = false;
+let valor = ""
+
+const calculadora = new Calculadora(displayPrevia, displayAtual)
+
+botoesNum.forEach((botao) => {
+    botao.addEventListener("click", (evento) => {
+        sinal = false;
+        valor = evento.target.innerText;
+        calculadora.adicionarDigito(valor)
+    })
+})
+
+botoesOperador.forEach((botao) => {
+    botao.addEventListener("click", (evento) => {
+        if(!sinal){
+            sinal = true;
+            valor = evento.target.innerText;
+            calculadora.adicionarDigito(valor)
+        }
+    })
+})
+
+botaoPonto.addEventListener("click", (evento) => {
+    valor = evento.target.innerText;
+    calculadora.adicionarDigito(valor)
+})
+
+botaoIgual.addEventListener("click", (evento) => {
+    calculadora.calcular()
+})
+
+botoesOperacao.forEach((botao) => {
+    botao.addEventListener("click", (evento) => {
+        valor = evento.target.innerText;
+        calculadora.processarOperacoes(valor)
+    })
+})
+
+// operadores são adicionados sem números antes
+// ponto
+// del apaga resultado
