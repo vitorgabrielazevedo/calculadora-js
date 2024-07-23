2a9db2579875e79cbee365e99f20f9612e21647c
Vitor Gabriel <vs569119@gmail.com>
2024-07-23 20:01:20 -0300

    refazendo o script

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,102 +1,140 @@
-const displayPrevia = document.querySelector("#previa");
-const displayAtual = document.querySelector("#operacao-atual");
-const botaoIgual = document.querySelector(".igual");
-const botaoPonto = document.querySelector(".ponto");
-const botoesNum = document.querySelectorAll(".num");
-const botoesOperador = document.querySelectorAll(".operador");
-const botoesOperacao = document.querySelectorAll(".operacao");
+// selecionando os elementos
+const displayPreview = document.querySelector("#display-preview");
+const displayAtual = document.querySelector("#display-atual");
+const numBtn = document.querySelectorAll(".num");
+const opBtn = document.querySelectorAll(".operador");
+const resultBtn = document.querySelector(".igual");
+
+// classe
 
 class Calculadora {
-  constructor(displayPrevia, displayAtual) {
-    this.displayPrevia = displayPrevia;
+  constructor(displayPreview, displayAtual) {
+    this.displayPreview = displayPreview;
     this.displayAtual = displayAtual;
     this.operacaoAtual = "";
   }
 
-  adicionarDigito(digito) {
-    if (digito === "." && this.displayAtual.innerText === "") {
-      digito = "0.";
-    }
+  atualizarDisplay() {
+    this.displayPreview += this.displayAtual;
+  }
 
+  adicionarDigito(digito) {
     this.operacaoAtual = digito;
     this.atualizarDisplay();
   }
-
-  calcular() {
-    let resultado = eval(this.displayAtual.innerText);
-    this.displayPrevia.innerText = this.displayAtual.innerText;
-    this.displayAtual.innerText = resultado;
-  }
-
-  processarDelOp() {
-    this.displayAtual.innerText = this.displayAtual.innerText.slice(0, -1);
-    this.displayPrevia.innerText = this.displayPrevia.innerText.slice(0, -1);
-  }
-
-  processarCOp() {
-    this.displayAtual.innerText = "";
-  }
-
-  processarCEOp() {
-    this.displayAtual.innerText = "";
-    this.displayPrevia.innerText = "";
-  }
-
-  processarOperacoes(valor) {
-    if (valor === "DEL") {
-      this.processarDelOp();
-    } else if (valor === "C") {
-      this.processarCOp();
-    } else {
-      this.processarCEOp();
-    }
-  }
-
-  atualizarDisplay() {
-    this.displayAtual.innerText += this.operacaoAtual;
-  }
 }
 
-let sinal = false;
-let decimal = false;
-let valor = "";
+const calculadora = new Calculadora(displayPreview, displayAtual);
 
-const calculadora = new Calculadora(displayPrevia, displayAtual);
+// eventos
 
-botoesNum.forEach((botao) => {
-  botao.addEventListener("click", (evento) => {
-    sinal = false;
-    valor = evento.target.innerText;
-    calculadora.adicionarDigito(valor);
+numBtn.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    const digito = e.target.innerText;
+
+    calculadora.adicionarDigito(digito);
   });
 });
 
-botoesOperador.forEach((botao) => {
-  botao.addEventListener("click", (evento) => {
-    if (!sinal) {
-      sinal = true;
-      decimal = false;
-      valor = evento.target.innerText;
-      calculadora.adicionarDigito(valor);
-    }
-  });
-});
+// const displayPrevia = document.querySelector("#previa");
+// const displayAtual = document.querySelector("#operacao-atual");
+// const botaoIgual = document.querySelector(".igual");
+// const botaoPonto = document.querySelector(".ponto");
+// const botoesNum = document.querySelectorAll(".num");
+// const botoesOperador = document.querySelectorAll(".operador");
+// const botoesOperacao = document.querySelectorAll(".operacao");
 
-botaoPonto.addEventListener("click", (evento) => {
-  if (!decimal) {
-    decimal = true;
-    valor = evento.target.innerText;
-    calculadora.adicionarDigito(valor);
-  }
-});
+// class Calculadora {
+//   constructor(displayPrevia, displayAtual) {
+//     this.displayPrevia = displayPrevia;
+//     this.displayAtual = displayAtual;
+//     this.operacaoAtual = "";
+//   }
 
-botaoIgual.addEventListener("click", (evento) => {
-  calculadora.calcular();
-});
+//   adicionarDigito(digito) {
+//     if (digito === "." && this.displayAtual.innerText === "") {
+//       digito = "0.";
+//     }
 
-botoesOperacao.forEach((botao) => {
-  botao.addEventListener("click", (evento) => {
-    valor = evento.target.innerText;
-    calculadora.processarOperacoes(valor);
-  });
-});
+//     this.operacaoAtual = digito;
+//     this.atualizarDisplay();
+//   }
+
+//   calcular() {
+//     let resultado = eval(this.displayAtual.innerText);
+//     this.displayPrevia.innerText = this.displayAtual.innerText;
+//     this.displayAtual.innerText = resultado;
+//   }
+
+//   processarDelOp() {
+//     this.displayAtual.innerText = this.displayAtual.innerText.slice(0, -1);
+//     this.displayPrevia.innerText = this.displayPrevia.innerText.slice(0, -1);
+//   }
+
+//   processarCOp() {
+//     this.displayAtual.innerText = "";
+//   }
+
+//   processarCEOp() {
+//     this.displayAtual.innerText = "";
+//     this.displayPrevia.innerText = "";
+//   }
+
+//   processarOperacoes(valor) {
+//     if (valor === "DEL") {
+//       this.processarDelOp();
+//     } else if (valor === "C") {
+//       this.processarCOp();
+//     } else {
+//       this.processarCEOp();
+//     }
+//   }
+
+//   atualizarDisplay() {
+//     this.displayAtual.innerText += this.operacaoAtual;
+//   }
+// }
+
+// let sinal = false;
+// let decimal = false;
+// let valor = "";
+
+// const calculadora = new Calculadora(displayPrevia, displayAtual);
+
+// botoesNum.forEach((botao) => {
+//   botao.addEventListener("click", (evento) => {
+//     sinal = false;
+//     valor = evento.target.innerText;
+//     calculadora.adicionarDigito(valor);
+//   });
+// });
+
+// botoesOperador.forEach((botao) => {
+//   botao.addEventListener("click", (evento) => {
+//     if (!sinal) {
+//       sinal = true;
+//       decimal = false;
+//       valor = evento.target.innerText;
+//       calculadora.adicionarDigito(valor);
+//     }
+//   });
+// });
+
+// botaoPonto.addEventListener("click", (evento) => {
+//   if (!decimal) {
+//     decimal = true;
+//     valor = evento.target.innerText;
+//     calculadora.adicionarDigito(valor);
+//   }
+// });
+
+// botaoIgual.addEventListener("click", (evento) => {
+//   calculadora.calcular();
+// });
+
+// botoesOperacao.forEach((botao) => {
+//   botao.addEventListener("click", (evento) => {
+//     valor = evento.target.innerText;
+//     calculadora.processarOperacoes(valor);
+//   });
+// });
